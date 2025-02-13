03b203abb0f0a40e42b91e6f4210e17f69e95ff8
bryan-ilman-on-github <bryanilman20@gmail.com>
2025-02-13 21:30:20 +0700

    modif benchmark func.

--- api/handler/benchmark.go
+++ api/handler/benchmark.go
@@ -1,9 +1,16 @@
 package handler
 
 import (
+	"context"
+	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"bearlysocial-backend/util"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func Benchmark(w http.ResponseWriter, r *http.Request) {
@@ -13,5 +20,28 @@ func Benchmark(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	data := make([]byte, 128*1024) // 128 KB.
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(data) // Fill with random data.
+
+	doc := bson.M{"_id": "benchmark_data", "data": data}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	opts := options.Update().SetUpsert(true)
+	_, err = util.MongoCollection.UpdateOne(ctx, bson.M{"_id": "benchmark_data"}, bson.M{"$set": doc}, opts)
+	if err != nil {
+		util.ReturnMessage(w, http.StatusInternalServerError, "Failed to insert/update data.")
+		return
+	}
+
+	var result bson.M
+	err = util.MongoCollection.FindOne(ctx, bson.M{"_id": "benchmark_data"}).Decode(&result)
+	if err != nil {
+		util.ReturnMessage(w, http.StatusInternalServerError, "Failed to retrieve data.")
+		return
+	}
+
+	fmt.Fprintln(w, "Benchmark successful.")
 	w.WriteHeader(http.StatusOK)
 }
